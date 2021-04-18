b173a37b4beef9ce91ada62b141365f746aea15b
Gustavo <gustavo@bgcbrasil.com.br>
2021-04-18 20:20:38 -0300

    DFS now find fathers and levels

--- src/functions/array/bfs.js
+++ src/functions/array/bfs.js
@@ -1,15 +1,42 @@
 /**
+* @name calculateMinimumPath
+* @description Calculates minimum path and its length.
+* @param {Number} targetNode - final vertice
+* @param {Number} sourceNode - initial vertice
+* @param {Number[]} fathers - Array of node's fathers
+*/
+function calculateMinimumPath(targetNode, sourceNode, fathers) {
+  const minimumPath = [];
+  let currentNode = targetNode;
+  while (currentNode !== sourceNode) {
+    minimumPath.push(currentNode);
+    currentNode = fathers[currentNode];
+  }
+
+  return {
+    minimumPathSize: minimumPath.length,
+    minimumPath: minimumPath.reverse(),
+  };
+}
+
+/**
 * @name bfs
 * @description Runs Breadth First Search on a graph.
 * @param {Number} s - start vertice
 * @param {Set[]} graph - Graph
 */
-export function bfs(s, graph) {
+export function bfs(s, graph, targetNode) {
   const size = graph.length;
   const visited = new Array(size + 1);
 
   let queue = [s];
+  const fathers = new Array(graph.length + 1);
+  const levels = new Array(graph.length + 1);
+  levels[s] = 0;
   visited[s] = true;
+  let foundNode;
+  let minimumPath;
+  let minimumPathSize;
 
   while (queue.length) {
     ([s, ...queue] = queue);
@@ -18,7 +45,22 @@ export function bfs(s, graph) {
       if (!visited[node]) {
         queue.push(node);
         visited[node] = true;
+        fathers[node] = s;
+        levels[node] = levels[s] + 1;
+        if (targetNode && node === targetNode) break;
       }
     }
+
+    if (targetNode && foundNode) break;
   }
+
+  if (targetNode && foundNode) ({ minimumPath, minimumPathSize } = calculateMinimumPath(targetNode, s, fathers));
+
+  return {
+    minimumPath,
+    foundNode,
+    fathers,
+    minimumPathSize,
+    levels,
+  };
 }
