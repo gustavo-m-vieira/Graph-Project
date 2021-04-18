c462f3444e6fdbb478cfff8e66ed1a0a36722a8d
Gustavo <gustavo@bgcbrasil.com.br>
2021-04-18 19:54:57 -0300

    reorganizing main and add some properties to Graph class

--- src/classes/Graph.js
+++ src/classes/Graph.js
@@ -6,13 +6,28 @@ import {
   getDegrees,
 } from '../functions';
 
+/**
+* @name Graph
+* @description A class that represents a Graph.
+* @param {Object} event
+* @param {'matrix' , 'array'} memoryStructure - if should represents as an array or a matrix
+* @param {String} [filePath] - path to the file
+* @param {Buffer} [Buffer] - Buffer of informations
+*/
 export class Graph {
   constructor({ memoryStructure, filePath, buffer }) {
     const Buffer = buffer || fs.readFileSync(filePath);
 
     this.edges = catchEdges(Buffer);
+    this.saveDegreesInfos();
+    this.saveFunctions(memoryStructure);
 
+    this.GraphStructure = this.createGraph(this.edges);
+  }
+
+  saveFunctions(memoryStructure) {
     let functions;
+
     switch (memoryStructure) {
       case 'array':
         functions = GraphAsArray;
@@ -25,9 +40,6 @@ export class Graph {
     }
 
     Object.entries(functions).forEach(([key, value]) => { this[key] = value; });
-
-    this.GraphStructure = this.createGraph(this.edges);
-    this.saveDegreesInfos();
   }
 
   saveDegreesInfos() {
@@ -42,5 +54,17 @@ export class Graph {
     this.highestDegree = highestDegree;
     this.medianDegree = medianDegree;
     this.averageDegree = averageDegree;
+  }
+
+  saveGraphInfosFile(path = './src/testFiles/testAnswerFiles/graphInfos.txt') {
+    let fileAsString = '';
+    fileAsString += `\nNº Nodes = ${this.GraphStructure.length}`;
+    fileAsString += `\nNº Edges = ${this.edges.length}`;
+    fileAsString += `\nLowest Degree = ${this.lowestDegree}`;
+    fileAsString += `\nHighest Degree = ${this.highestDegree}`;
+    fileAsString += `\nMedian Degree = ${this.medianDegree}`;
+    fileAsString += `\nAverage Degree = ${this.averageDegree}`;
+
+    fs.writeFileSync(path, fileAsString);
   }
 }
