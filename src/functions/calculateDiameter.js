c1237e54f450ef519929613dc4ed53a899f1b975
Pedro Gomes <pedro.gomes@poli.ufrj.br>
2021-04-18 23:04:03 -0300

    Finding connected components

--- src/functions/calculateDiameter.js
+++ src/functions/calculateDiameter.js
@@ -1,6 +1,6 @@
 export function calculateDiameter(graph, func) {
   const biggestLevels = new Array(graph.length);
-  for (let node = 1; node <= graph.length; node += 1) {
+  for (let node = 1; node < graph.length; node += 1) {
     const { levels } = func(node, graph);
     biggestLevels.push(Math.max.apply(null, levels));
   }
