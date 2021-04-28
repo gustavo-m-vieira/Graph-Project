ef94faadcc3a76e41c18e84cabdd5991f58d526b
Pedro Gomes <pedro.gomes@poli.ufrj.br>
2021-04-28 04:08:49 -0300

    Fixed repeated occurencies of the same edge -> to be optimized

--- src/functions/catchEdges.js
+++ src/functions/catchEdges.js
@@ -3,14 +3,33 @@
 * @description Receives a Buffer and returns an array with edges.
 * @param {Buffer} Buffer - a Buffer
 */
+
+// Redefining prototype property 'equals' to be able to compare arrays
+// In this case, length is always 2 (goal is to compare an array of edges) so this approach is cheap.
+// eslint-disable-next-line no-extend-native
+Array.prototype.equals = (array) => {
+  if (!array) return false;
+  if (this.length !== array.length) return false;
+  for (let index = 0; index < this.length; index += 1) {
+    if (this[index] !== array[index]) return false;
+  }
+  return true;
+};
+
 export function catchEdges(Buffer) {
   const bufferAsString = Buffer.toString();
   const [qtdNodes, ...nestedEdges] = bufferAsString.split('\n');
 
-  const edges = nestedEdges.map((edge) => edge.split(' ').map((node) => parseInt(node, 10)));
+  let edges = nestedEdges.map((edge) => edge.split(' ').map((node) => parseInt(node, 10)));
+  edges = edges.map((edge) => edge.sort());
+  // current solution to remove repeated arrays in edges (EXPENSIVE)
+  const edgesWithNorepetition = Array.from(new Set(edges.map(JSON.stringify)), JSON.parse);
 
+  // Possible better solution using the overwritten Array.equals method: UNDER DEVELOPMENT!!
+  // let edgesWithNoRepetition = edges.reduce((accumulator, currentArray) => {
+  // }, []);
   return {
-    edges: edges.filter(([node1, node2]) => Number.isInteger(node1) && Number.isInteger(node2)),
+    edges: edgesWithNorepetition.filter(([node1, node2]) => Number.isInteger(node1) && Number.isInteger(node2)),
     qtdNodes,
   };
 }
