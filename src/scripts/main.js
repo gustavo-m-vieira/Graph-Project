7d8651ba3b5d37091bba4612ab40b8a602584198
Pedro Gomes <pedro.gomes@poli.ufrj.br>
2021-04-28 19:46:16 -0300

    Study of 6 cases: 6 graphs to get their information

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,18 +3,18 @@ import util from 'util';
 import { Graph } from '../classes';
 
 util.inspect.defaultOptions.depth = null;
-const { path = './src/testFiles/testInputFiles/testesimples.txt' } = minimist(process.argv.slice(2));
+const { path = './src/testFiles/testInputFiles/grafo_3.txt' } = minimist(process.argv.slice(2));
 
 /**
 * @name Main
 * @description Main Function.
-* @command npx -p @babel/core -p @babel/node babel-node --presets @babel/preset-env ./src/scripts/main.js --path {PATH}
+* @command npx -p @babel/core -p @babel/node babel-node --presets @babel/preset-env ./src/scripts/main.js
 */
 async function main() {
   try {
     const graph = new Graph({ memoryStructure: 'adjacent matrix', filePath: path });
 
-    console.log({ graph });
+    // console.log({ graph });
 
     graph.saveGraphInfosFile();
   } catch (error) {
